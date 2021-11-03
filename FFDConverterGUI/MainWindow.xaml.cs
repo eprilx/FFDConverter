f82fad7b794017d1b9cfdc5903e8898fdfe570d5
eprilx <ducan01245@gmail.com>
2021-11-04 00:05:00 +0700

    add autoupdate

--- FFDConverterGUI/MainWindow.xaml.cs
+++ FFDConverterGUI/MainWindow.xaml.cs
@@ -44,8 +44,6 @@ namespace FFDConverterGUI
     {
         public MainWindow()
         {
-            //AutoUpdater.UpdateFormSize = new System.Drawing.Size(900, 600);
-
             // Change current culture
             CultureInfo culture;
             culture = CultureInfo.CreateSpecificCulture("en-US");
@@ -59,9 +57,9 @@ namespace FFDConverterGUI
                 ToolVersion = ToolVersion.Remove(ToolVersion.Length - 2);
                 Hide();
                 AutoUpdater.Synchronous = true;
-                AutoUpdater.InstalledVersion = new Version("1.0");
-                AutoUpdater.UpdateFormSize = new System.Drawing.Size((int)SystemParameters.FullPrimaryScreenHeight / 2, (int)SystemParameters.FullPrimaryScreenWidth / 2);
-                AutoUpdater.Start("https://raw.githubusercontent.com/eprilx/FFDConverter/dev/AutoUpdate.xml");
+                AutoUpdater.InstalledVersion = new Version(ToolVersion);
+                AutoUpdater.UpdateFormSize = new System.Drawing.Size(1200, 800);
+                AutoUpdater.Start("https://raw.githubusercontent.com/eprilx/FFDConverter/master/AutoUpdate.xml");
             }
             catch
             {
